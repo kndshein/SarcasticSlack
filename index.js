e5de1ca9ab6ca53cc204fd8fb0c0aba35a9c1a2b
Kaung Shein <nicholasling1412@gmail.com>
2021-12-26 21:43:10 -0800

    Fixed workaround for verifying token

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const { WebClient } = require("@slack/web-api");
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const { createHmac, timingSafeEqual } = require("crypto");
+const querystring = require("querystring");
+
 const app = express();
+const bodyParser = require("body-parser");
 const web = new WebClient(process.env.BOT_USER_OAUTH_TOKEN);
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
@@ -19,18 +21,37 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(function (req, res, next) {
-  console.log(
-    Object.entries(req.body)
-      .map((ele, idx) => [(ele[0] += "="), ele[1]])
-      .flat()
-      .join("&")
-  );
-  next();
+  let encodedReq = querystring.stringify(req.body);
+  encodedReq = encodedReq.replace("%20", "+"); //TODO: Replace the brittle workaround here.
+  let slackTimestamp = req.headers["x-slack-request-timestamp"];
+  let slackSignature = req.headers["x-slack-signature"];
+  if (Math.round(Date.now() / 1000) - slackTimestamp > 300) {
+    res.json({
+      status: 425,
+      text: `Oops, looks like chef's out for a smoke break. (Don't smoke, kids) Err code: Too Early or too late`,
+    });
+    return;
+  }
+  let baseString = `v0:${slackTimestamp}:${encodedReq}`;
+  let signature =
+    "v0=" +
+    createHmac("sha256", process.env.SIGNING_SECRET)
+      .update(baseString)
+      .digest("hex");
+  if (timingSafeEqual(Buffer.from(slackSignature), Buffer.from(signature))) {
+    next();
+  } else {
+    res.json({
+      status: 401,
+      text: `Oops, either you're trying to steal the sauce recipe or the dev has botched the code.`,
+    });
+  }
 });
 
 app.post("/mild", async (req, res) => {
-  console.log(req.body);
   const { token, response_url, channel_id, channel_name, user_id, text } =
     req.body;
   try {
@@ -60,6 +81,7 @@ app.post("/mild", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.json({
+      status: 500,
       text: `Oops, looks like chef's out for a smoke break. (Don't smoke, kids) Err code: ${error.data.error}`,
     });
   }
